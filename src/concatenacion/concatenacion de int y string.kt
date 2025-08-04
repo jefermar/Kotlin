3e63ba2b7d95244845eb6eff8624de3e4d1b92b4
jefermar <jefermar17jjs@gmail.com>
2025-08-04 16:23:31 -0500

    Kotlin concatenacion

---
+++ src/concatenacion/concatenacion de int y string.kt
@@ -0,0 +1,10 @@
+package concatenacion
+
+fun main (){
+    val introduction = "El resultado de"
+    val plus = "más"
+    val firstNumber = 2
+    val secondNumber = 5
+
+    println("$introduction $firstNumber $plus $secondNumber es: ${firstNumber + secondNumber}")
+}
