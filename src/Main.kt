518d9cecdd2bba86940cfe11a341efb5fc80f93f
jefermar <jefermar17jjs@gmail.com>
2025-08-04 14:55:21 -0500

    create Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -6,4 +6,16 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello primera prueba de: " + name + "!")
 
+    var numeroFavorito: Int = -231
+    //var numeroFavorito: Long = 474747474
+    //var numeroFavorito: Float = 1.93f
+    //var numeroFavorito: Double = 1.99993
+    //var numeroFavorito: Char = '1'
+    var letraFavorita: Char = 'q'
+    var numeroFavoritoS: Char = '@'
+    var test: String = "test. 12345! .$%&/"
+    var estoyTriste: Boolean = false
+    var estoyFeliz: Boolean = true
+
+    println(numeroFavorito)
 }
